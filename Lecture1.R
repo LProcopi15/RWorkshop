4624948907344293d44b886defaacdef728b5509
LProcopi15 <lep9cf@virginia.edu>
2017-01-29 18:05:41 -0500

    Actually final code for lecture 1

--- Lecture1.R
+++ Lecture1.R
@@ -4,9 +4,8 @@
 #
 #######################
 
-# Data types and basic visualizations
+### Data Types
 
-# Data Types
 # 1. int - integer whole numbers 
 # put an L after the number to ensure to is read as an integer
 type <- 2L
@@ -37,23 +36,60 @@ bike$weekday[which((bike$weekday)    == 4)] <- "Thr"
 bike$weekday[which((bike$weekday)    == 5)] <- "Fri"  
 print(class(bike$weekday))
 bike$weekday <- as.factor(bike$weekday)
-# Change month to a factor
-# Change season to a factor
-# Change dteday to a date element
-bike$dteday <- as.Date.character(bike$dteday, format = "%m/%d/%Y")
-?as.Date
-# Change holiday to a logic element
-str(bike$holiday)
-bike$holiday <- as.logical(bike$holiday)
 
-# Basic visualizations
+#### Basic visualizations
 str(bike)
 # histogram
 hist(bike$cnt)
 # box plot
 boxplot(bike$cnt)
+boxplot(cnt~weathersit,data=bike, main="Bike Rentals", 
+        xlab="Weather Classification", ylab="Total Count of Bikes")
 # bar
 barplot(table(bike$weathersit))
 
+### String manipulation
+bike$weathersit[which((bike$weathersit)    == 1)] <- "      THIS WEATHER is awesome"  
+bike$weathersit[which((bike$weathersit)    == 2)] <- "this weather is okay     "  
+bike$weathersit[which((bike$weathersit)    == 3)] <- "   this weather is not great   "
+bike$weathersit[which((bike$weathersit)    == 4)] <- "Yup THIS WEATHER IS THE WORST"
+bike$weathersit <- as.factor(bike$weathersit)
+summary(bike$weathersit)
+
+# First we have to install a package which enables us to use the desired function
+install.packages('stringr')
+# We only need to install the package once, but need to load the library every time
+library(stringr)
+# Now we can manipulate these strings
+bike$weathersit <- str_trim(bike$weathersit)
+bike$weathersit <- toupper(bike$weathersit) 
+bike$weathersit <- tolower(bike$weathersit)
+
+# Character matching
+grepl("worst", bike$weathersit)
+grep("worst", bike$weathersit)
+grep("^Y", bike$weathersit) # Does not return the proper thing? 
 
 
+#############################
+#
+# Practice Problems
+#
+#############################
+# ***IMPORTANT NOTE: to change more data types into a different one the general 
+# command is as.datatype (ex. as.factor, as.Date). Also, if you are confused about
+# the meaning or the input of a command you can use the following format:
+?as.Date
+
+# 1a) Change month to a factor with the levels Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sept, Oct, Nov, Dec
+# 1b) Change season to a factor with the levels Win, Spr, Sum, Fall
+# 1c) Change dteday to a date element
+  # Note: be sure to check that the dates were processed correctly
+
+# 2a) Create a boxplot of the count of casual users
+# 2b) What is the median?
+# 2c) What is the 50% CCI? 
+
+# 3a) Change all the days of the week to capital letters
+# 3b) Create boxplots that compare the total count of bike rentals for when the 
+# day of the week starts with F and when it does not
