33f2e456984e30a13ccd5e93156542153b56e0f6
LProcopi15 <lep9cf@virginia.edu>
2017-01-29 16:57:29 -0500

    Final version of Lecture 1

--- Lecture1.R
+++ Lecture1.R
@@ -20,10 +20,40 @@ print(type)
 print(class(type))
 # 6. Matrices
 type <- matrix(c('a', 'b', 'c', 'd'), nrow = 2, ncol = 2, byrow = TRUE)
-# 7. Factors
-View(airquality)
-summary(airquality)
-str(airquality)
+# 7. Factors - a vector with distinct values of the elements
+bike <- read.csv("C:/Users/Student/Documents/UVA 2016-2017/RWorkshop/bike.csv")
+View(bike)
+summary(bike)
+str(bike)
+
+# Change day of week to factor
+str(bike$weekday)
+bike$weekday[which((bike$weekday)    == 6)] <- "Sat"  
+bike$weekday[which((bike$weekday)    == 0)] <- "Sun"  
+bike$weekday[which((bike$weekday)    == 1)] <- "Mon"  
+bike$weekday[which((bike$weekday)    == 2)] <- "Tue"  
+bike$weekday[which((bike$weekday)    == 3)] <- "Wed"  
+bike$weekday[which((bike$weekday)    == 4)] <- "Thr"  
+bike$weekday[which((bike$weekday)    == 5)] <- "Fri"  
+print(class(bike$weekday))
+bike$weekday <- as.factor(bike$weekday)
+# Change month to a factor
+# Change season to a factor
+# Change dteday to a date element
+bike$dteday <- as.Date.character(bike$dteday, format = "%m/%d/%Y")
+?as.Date
+# Change holiday to a logic element
+str(bike$holiday)
+bike$holiday <- as.logical(bike$holiday)
+
+# Basic visualizations
+str(bike)
+# histogram
+hist(bike$cnt)
+# box plot
+boxplot(bike$cnt)
+# bar
+barplot(table(bike$weathersit))
 
 
 
